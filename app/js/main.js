106ad6d254ae5d232e82dac9ca7aa46409e28618
Mardvey <stefan.irk96@mail.ru>
2020-03-25 12:48:36 +0800

    added section city

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,31 @@
 $(function() {
-  
+  $('.city__inner').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 1500,
+    arrows: false,
+    fade: true,
+    asNavFor: '.city__smallslider',
+    draggable: false,
+    // afterChange: slick, currentSlide,
+  });
+
+  $('.city__smallslider').slick({
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    asNavFor: '.city__inner',
+    arrows: false,
+    speed: 1500,
+    centerMode: true,
+    focusOnSelect: true,
+    centerPadding: '60px',
+    
+  });
+
+
+  $(".city__smallslider .city__items").click(function () {
+    $(".city__smallslider .city__items").removeClass("active");
+    $(this).toggleClass("active");
+  })
+
 });

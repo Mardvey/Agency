84a0264cb93a694d10b9cb24e6d571ba1017d374
Mardvey <stefan.irk96@mail.ru>
2020-03-28 21:01:17 +0800

    added section relaxation

--- app/js/main.js
+++ app/js/main.js
@@ -7,7 +7,6 @@ $(function() {
     fade: true,
     asNavFor: '.city__smallslider',
     draggable: false,
-    // afterChange: slick, currentSlide,
   });
 
   $('.city__smallslider').slick({
@@ -18,13 +17,41 @@ $(function() {
     speed: 1500,
     centerMode: true,
     focusOnSelect: true,
-    centerPadding: '60px',
+    // centerPadding: '60px',
+    centerPadding: '-10px'
     
   });
+
+  $('.relaxation__top-slider').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: '.relaxation__down',
+    arrows: false,
+    speed: 1500,
+    centerMode: true,
+    focusOnSelect: true,
+    centerPadding: '0px',
+  });
+
+  $('.relaxation__down').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 1500,
+    arrows: false,
+    fade: true,
+    asNavFor: '.relaxation__top-slider',
+    draggable: false,
+  });
+
 
 
   $(".city__smallslider .city__items").click(function () {
     $(".city__smallslider .city__items").removeClass("active");
+    $(this).toggleClass("active");
+  })
+
+  $(".relaxation__top-slider .relaxation__item").click(function () {
+    $(".relaxation__top-slider .relaxation__item").removeClass("active");
     $(this).toggleClass("active");
   })
 
